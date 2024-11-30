e1f541fd46c1061b755d6d9ad791b6c680a2473f
camwalford <cwalford2012@gmail.com>
2024-11-29 18:50:35 -0800

    Edited labeller to return flattened and unflattened landmarks to reduce computation in movement detection

--- src/jetson/labeller.py
+++ src/jetson/labeller.py
@@ -1,33 +1,33 @@
-import cv2
 import mediapipe as mp
 import numpy as np
-import os
-import keras
-from collections import deque
+
 
 class Labeller:
 
     def __init__(self):
         print("Setting up Blaze.")
         self.pose = mp.solutions.pose.Pose(static_image_mode=False,
-                             model_complexity=1,
-                             smooth_landmarks=True,
-                             enable_segmentation=False,
-                             min_detection_confidence=0.5,
-                             min_tracking_confidence=0.5)
-
+                                           model_complexity=1,
+                                           smooth_landmarks=True,
+                                           enable_segmentation=False,
+                                           min_detection_confidence=0.5,
+                                           min_tracking_confidence=0.5)
 
     def extract_landmarks(self, image):
-        print("Labeller received image:", image)
+        # print("Extracting landmarks.")
+
         results = self.pose.process(image)
         if not results.pose_landmarks:
-            return np.zeros(99)  # Return a zero array if no landmarks are detected
+            return None, None
 
-        print("Extracting landmarks.")
         landmarks = results.pose_landmarks.landmark
         pose = []
         for landmark in landmarks:
-            pose.extend([landmark.x, landmark.y, landmark.z])
+            pose.append([landmark.x, landmark.y, landmark.z])
 
-        print("Returning landmarks.")
-        return np.array(pose)
+        non_flattened_landmarks = np.array(pose)  # Shape: (33, 3)
+        flattened_landmarks = non_flattened_landmarks.flatten()  # Shape: (99,)
+
+        # print(f"Shape of flattened_landmarks: {flattened_landmarks.shape}")
+        # print(f"Shape of non_flattened_landmarks: {non_flattened_landmarks.shape}")
+        return flattened_landmarks, non_flattened_landmarks
